65bb45d571388b153dad970a6f6fe9ecf97c6371
why <wuhaoyu2222@gmail.com>
2019-11-24 16:44:02 +0800

    二叉树的下一个节点

---
+++ src/Solution8.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Solution8 {
+    public TreeLinkNode GetNext(TreeLinkNode pNode){
+        TreeLinkNode pNext = null;
+        if (pNode == null){
+            return pNext;
+        }
+        if (pNode.right != null) {
+            TreeLinkNode pRight = pNode.right;
+            while (pRight.left != null) {
+                pRight = pRight.left;
+            }
+            pNext = pRight;
+        } else if (pNode.next != null) {
+            TreeLinkNode pCurrent = pNode;
+            TreeLinkNode pParent = pNode.next;
+            while (pParent != null && pCurrent == pParent.right) {
+                pCurrent = pParent;
+                pParent = pParent.next;
+            }
+            pNext = pParent;
+        }
+        return pNext;
+    }
+}
