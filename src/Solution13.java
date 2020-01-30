a4a7e5efb9ebe714c555b0910270300a93727c46
why <wuhaoyu2222@gmail.com>
2020-01-30 10:05:47 +0800

    机器人的运动范围

---
+++ src/Solution13.java
@@ -0,0 +1,53 @@
+public class Solution13 {
+    public static int movingCount(int threshold, int rows, int cols) {
+        int count;
+        if (threshold < 0 || rows <= 0 || cols <= 0) {
+            return 0;
+        }
+        boolean[] visited = new boolean[rows * cols];
+        for (int i = 0; i < rows * cols; ++i) {
+            visited[i] = false;
+        }
+        count = movingCountCore(threshold, rows, cols, 0, 0, visited);
+        return count;
+    }
+
+    public static int movingCountCore(int threshold, int rows, int cols, int row, int col, boolean[] visited) {
+        int count = 0;
+        if (check(threshold, rows, cols, row, col, visited)) {
+            count = 1 + movingCountCore(threshold, rows, cols, row - 1, col, visited)
+                    + movingCountCore(threshold, rows, cols, row + 1, col, visited)
+                    + movingCountCore(threshold, rows, cols, row, col - 1, visited)
+                    + movingCountCore(threshold, rows, cols, row, col + 1, visited);
+        }
+        return count;
+    }
+
+    /**
+     * 用来判断机器人能否进入坐标为(row,col)的方格。
+     * @param threshold:k
+     * @param rows
+     * @param cols
+     * @param row：横坐标
+     * @param col：纵坐标
+     * @param visited：已经被访问,是一个数组，false表示没有被访问过，true表示已经被访问过
+     * @return
+     */
+    public static boolean check(int threshold, int rows, int cols, int row, int col, boolean[] visited) {
+        if (row >= 0 && col >= 0 && row < rows && col < cols
+                && getDigitSum(row + col) <= threshold
+                && !visited[row * cols + col]) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int getDigitSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -5,17 +5,9 @@
  */
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {6501, 6828, 6963, 7036,
-                7422, 7674, 8146, 8468, 8704,
-                8717, 9170, 9359, 9719, 9895,
-                9896, 9913, 9962, 154, 293,
-                334, 492, 1323, 1479, 1539,
-                1727, 1870, 1943, 2383, 2392,
-                2996, 3282, 3812, 3903, 4465,
-                4605, 4665, 4772, 4828, 5142,
-                5437, 5448, 5668, 5706, 5725,
-                6300, 6335};
-        int[] arr2 = {3, 4, 5, 1, 2};
-        System.out.println(Solution11.minNumberInRotateArray2(arr2));
+        int threshold = 4;
+        int rows = 3;
+        int cols = 4;
+        Solution13.movingCount(threshold, rows, cols);
     }
 }
